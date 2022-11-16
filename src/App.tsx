e856d1b7fb3bcc8b02a7c0115fa414efc0001189
Chiqo Rifky Saputra <bandi_nada@live.com>
2022-11-16 21:51:49 +0700

    feat: add component for post and navbar at homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import Post from './components/Post';
+import Navbar from './components/ui/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <div className="container mx-auto px-16 py-2">
+        <Post />
+      </div>
+    </>
   );
 }
 
